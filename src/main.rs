612163bd3d75021d88773928a882011945d9f5ac
dust-zed <1186669164@qq.com>
2025-02-08 15:43:01 +0800

    add print style

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use anstream::println;
+use anstyle::{Color, RgbColor, Style};
 use clap::Parser;
 use command::Args;
-use ignore::{types::{self, TypesBuilder}, Walk, WalkBuilder};
-use std::{error::Error, fs::File, io::{BufRead, BufReader, Error as IoError, Read}, path::Path};
+use ignore::{
+    types::{self, TypesBuilder},
+    Walk, WalkBuilder,
+};
+use regex::Regex;
 use std::io::Result as IoResult;
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader, Error as IoError, Read},
+    path::Path,
+};
 mod command;
 fn main() {
     let args = Args::parse();
@@ -10,7 +21,14 @@ fn main() {
 }
 
 fn run(args: Args) -> Result<(), Box<dyn Error>> {
-    let Args {path, name, content, max_depth, threads, ignore_case} = args;
+    let Args {
+        path,
+        name,
+        content,
+        max_depth,
+        threads,
+        ignore_case,
+    } = args;
     let mut types_builder = TypesBuilder::new();
     // 添加文件名的glob模式匹配
     if let Some(name) = name {
@@ -18,6 +36,8 @@ fn run(args: Args) -> Result<(), Box<dyn Error>> {
         types_builder.select("all");
     };
     let types = types_builder.build().unwrap();
+    //内容支持正则匹配
+    let regex = Regex::new(&content).expect("build regex error");
     let walk = WalkBuilder::new(path).types(types).build();
     for results in walk {
         match results {
@@ -27,22 +47,24 @@ fn run(args: Args) -> Result<(), Box<dyn Error>> {
                     continue;
                 } else if metadata.is_file() {
                     println!("{}", entry.path().display());
-                    search_content(&content, entry.path()).unwrap();
+                    search_content(&regex, entry.path()).unwrap();
                 } else {
                     continue;
                 }
-            },
-            Err(e) => return Err(Box::new(e))
+            }
+            Err(e) => return Err(Box::new(e)),
         }
     }
     Ok(())
 }
 
-fn search_content(keyword: &String, path: &Path) -> IoResult<()> {
-    println!("keyword is {}", keyword);
+fn search_content(pattern: &Regex, path: &Path) -> IoResult<()> {
+    println!("keyword is {}", pattern.as_str());
     let file = File::open(path)?;
     let mut read = BufReader::new(file);
     let mut buffer = String::new();
+    let green = anstyle::Style::new().fg_color(Some(Color::Rgb(RgbColor(0, 255, 0))));
+    let blue = anstyle::Style::new().fg_color(Some(Color::Rgb(RgbColor(0, 0, 255))));
     //行号
     let mut line_num = 0;
     loop {
@@ -50,14 +72,13 @@ fn search_content(keyword: &String, path: &Path) -> IoResult<()> {
         match read.read_line(&mut buffer) {
             Ok(0) => break,
             Ok(n) => {
-                if buffer.contains(keyword) {
-                    println!("{line_num}{}", buffer)
+                if pattern.is_match(&buffer) {
+                    println!("{}{}\t{}{}", green, line_num, blue, buffer);
                 }
                 buffer.clear();
-            },
+            }
             Err(e) => return Err(e),
         }
     }
     Ok(())
 }
-
